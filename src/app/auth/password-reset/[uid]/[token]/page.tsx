432acc1b02b3e5b7e93893f110d27486e38695cb
lemon <lemon@example.com>
2025-09-28 15:14:23 +0300

    commit

--- src/app/auth/password-reset/[uid]/[token]/page.tsx
+++ src/app/auth/password-reset/[uid]/[token]/page.tsx
@@ -1,12 +1,9 @@
 // ai-chat-next/src/app/auth/password-reset/[uid]/[token]/page.tsx
 import ResetPasswordForm from "@features/users/ResetPasswordForm";
 
-export default function Page({
-  params,
-}: {
-  params: { uid: string; token: string };
-}) {
-  const { uid, token } = params;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function Page(props: any) {
+  const { uid, token } = props.params;
   return <ResetPasswordForm uid={uid} token={token} />;
 }
 
