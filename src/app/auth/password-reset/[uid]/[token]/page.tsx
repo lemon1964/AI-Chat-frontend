d88f7e6b247388af1282803a47156b3d9dd2cae7
lemon <lemon@example.com>
2025-09-28 15:11:15 +0300

    commit

--- src/app/auth/password-reset/[uid]/[token]/page.tsx
+++ src/app/auth/password-reset/[uid]/[token]/page.tsx
@@ -1,13 +1,22 @@
 // ai-chat-next/src/app/auth/password-reset/[uid]/[token]/page.tsx
 import ResetPasswordForm from "@features/users/ResetPasswordForm";
 
-interface Props {
-    params: {
-      uid: string;
-      token: string;
-    };
+export default function Page({
+  params,
+}: {
+  params: { uid: string; token: string };
+}) {
+  const { uid, token } = params;
+  return <ResetPasswordForm uid={uid} token={token} />;
 }
-export default function Page(props: Props) {
-    const { uid, token } = props.params;
-    return <ResetPasswordForm uid={uid} token={token} />;
-  }
+
+// interface Props {
+//     params: {
+//       uid: string;
+//       token: string;
+//     };
+// }
+// export default function Page(props: Props) {
+//     const { uid, token } = props.params;
+//     return <ResetPasswordForm uid={uid} token={token} />;
+//   }
