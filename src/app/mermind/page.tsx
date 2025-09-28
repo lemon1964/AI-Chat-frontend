bafac601412e60c8d0d20c67048bbd3c890ba52b
lemon <lemon@example.com>
2025-09-28 19:10:01 +0300

    commit

--- src/app/mermind/page.tsx
+++ src/app/mermind/page.tsx
@@ -1,7 +1,13 @@
 // app/mermind/page.tsx
 "use client";
 import MermindContainer from "@features/mermind/MermindContainer";
+import { ErrorBoundary } from "@ui/common/ErrorBoundary";
 
 export default function Page() {
-  return <MermindContainer />;
+  return(
+    <ErrorBoundary>
+      <MermindContainer />
+    </ErrorBoundary>
+  )
+  // <MermindContainer />;
 }
